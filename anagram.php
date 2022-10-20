7ea08814c74c6bd0b3e21d37efbc1c8a9e9f9331
marius <marius.mestron@epitech.eu>
2022-10-20 16:57:05 +0200

    [ADD]: anagram check

--- anagram.php
+++ anagram.php
@@ -1,5 +1,26 @@
 <?php
+$wordlist = "anagram-master-dictionnaire.txt";
+
+if (isset($argv[1])) {
+    $arg = $argv[1];
+    if($stream = fopen($wordlist, "r")){
+        while($word = fgets($stream)){
+            is_anagram($arg, $word);
+        }
+        fclose($stream);
+    }
+} else {
+    echo "\e[0;31mPas d'arguments...\e[0m\n";
+}
 
 
+function is_anagram($a, $b)
+{
+    if (strlen($a)+1 == strlen($b)+1) {
+        echo $b;
+    } else {
+        return false;
+    }
+}
 
 ?>
